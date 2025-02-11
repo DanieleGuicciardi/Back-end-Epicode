552be2b53ce796701e43d522ac67d71b587f627b
Danieloun <dany.guicciardi@outlook.com>
2025-02-11 17:57:30 +0100

    Ex 1102

---
+++ Week1/L2-1102/L2-1102/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Person
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, string surname, int age)
+    {
+        Name = name;
+        Surname = surname;
+        Age = age;
+    }
+
+    public string GetName()
+    {
+        return Name;
+    }
+    public string GetSurname()
+    {
+        return Surname;
+    }
+    public int GetAge()
+    {
+        return Age;
+    }
+    
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person person = new Person("Daniele", "Guicciardi", 21);
+        
+       Console.WriteLine("Here is the name of the person:");
+       Console.WriteLine($"Name: {person.GetName()}");
+       Console.WriteLine($"Surname: {person.GetSurname()}");
+       Console.WriteLine($"Age: {person.GetAge()}");
+       Console.WriteLine($"Here are the complete information: ");
+    }
+}
